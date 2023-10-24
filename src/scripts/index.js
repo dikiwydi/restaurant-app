d1ed6110c434033cb04cfb0b3154540ec2474aa1
dikiwydi <dikiwahyudi268@gmail.com>
2023-10-24 13:33:12 +0700

    sub2

---
+++ src/scripts/index.js
@@ -0,0 +1,17 @@
+import 'regenerator-runtime';
+import '../styles/main.css';
+import App from './views/app.js';
+
+const app = new App({
+  button: document.querySelector('#menu'),
+  drawer: document.querySelector('#drawer'),
+  content: document.querySelector('main'),
+});
+
+window.addEventListener('hashchange', () => {
+  app.renderPage();
+});
+
+window.addEventListener('load', () => {
+  app.renderPage();
+});
